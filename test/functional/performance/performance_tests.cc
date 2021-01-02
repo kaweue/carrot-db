c3e40726cd7b52ffe112c3713720625757a9b665
Pawel Marchewka <pawel.marchewka@codilime.com>
2021-01-02 01:41:05 +0100

    Refactored peformance tests

--- test/functional/performance/performance_tests.cc
+++ test/functional/performance/performance_tests.cc
@@ -26,46 +26,73 @@ namespace
             carrot_db->stop();
             carrot_db.release();
             client.release();
+            tasks.clear();
         }
-        std::unique_ptr<http::client::http_client> client;
-        std::unique_ptr<app::carrot_db> carrot_db;
-    };
 
-    TEST_F(rest_test, posts_and_gets)
-    {
-        auto is_status_valid = [](web::http::status_code code) {
-            if (code == web::http::status_codes::NotFound || code == web::http::status_codes::OK)
-                return testing::AssertionSuccess();
-            else
-                return testing::AssertionFailure() << "unexpected status code = " << code;
+        void test_posts_and_gets_performance()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                send_post_and_gets_requests_batch(i);
+                wait_for_all_requests();
+            }
+        }
+
+        void send_post_and_gets_requests_batch(uint batch_number)
+        {
+            for (int request_number = 0; request_number < 100; request_number++)
+            {
+                sent_test_request(http::methods::POST, make_request_path(batch_number, request_number));
+                sent_test_request(http::methods::GET, make_request_path(batch_number, request_number));
+            }
+        }
+
+        std::string make_request_path(uint batch_number, uint request_number)
+        {
+            return "my-key-" + std::to_string(batch_number) + "-" + std::to_string(request_number);
+        }
+
+        void sent_test_request(http::method method, std::string key)
+        {
+            auto task = client->request(http::methods::GET, key);
+            task.then([&](http::http_response response) {
+                EXPECT_TRUE(assert_non_error_status_response(response));
+                return response.body().close();
+            });
+            tasks.push_back(task);
         };
 
-        auto test_start = std::chrono::high_resolution_clock::now();
-        for (int i = 0; i < 10; i++)
+        void wait_for_all_requests()
         {
-            std::vector<pplx::task<web::http::http_response>> tasks;
-            for (int j = 0; j < 100; j++)
-            {
-                auto post_response = client->request(http::methods::POST, "my-key-" + std::to_string(i) + "-" + std::to_string(j));
-                post_response.then([&](http::http_response response) {
-                    EXPECT_EQ(response.status_code(), web::http::status_codes::Created);
-                    return response.body().close();
-                });
-                tasks.push_back(post_response);
-                auto get_response = client->request(http::methods::GET, "my-key-" + std::to_string(i) + "-" + std::to_string(j));
-                //then not found response is issued
-                get_response.then([&](http::http_response response) {
-                    EXPECT_TRUE(is_status_valid(response.status_code()));
-                    return response.body().close();
-                });
-                tasks.push_back(get_response);
-            }
             for (auto task : tasks)
             {
                 task.wait();
             }
         }
 
+        testing::AssertionResult assert_non_error_status_response(http::http_response response)
+        {
+            auto status_code = response.status_code();
+            switch (status_code)
+            {
+            case web::http::status_codes::NotFound:
+            case web::http::status_codes::OK:
+            case web::http::status_codes::Created:
+                return testing::AssertionSuccess();
+            default:
+                return testing::AssertionFailure() << "unexpected status code = " << status_code;
+            }
+        }
+
+        std::unique_ptr<http::client::http_client> client;
+        std::unique_ptr<app::carrot_db> carrot_db;
+        std::vector<pplx::task<web::http::http_response>> tasks;
+    };
+
+    TEST_F(rest_test, posts_and_gets_performance)
+    {
+        auto test_start = std::chrono::high_resolution_clock::now();
+        test_posts_and_gets_performance();
         auto test_end = std::chrono::high_resolution_clock::now();
         auto test_duration = std::chrono::duration_cast<std::chrono::seconds>(test_end - test_start).count();
         EXPECT_LE(test_duration, 10);
